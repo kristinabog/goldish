eb890585889e1ddb1a81e1d9be1e82d193fa3e12
kristinabog <kristinabog7@hotmail.com>
2021-11-01 23:17:35 +0000

    Product Admin - Added edit product template and functionality

--- products/views.py
+++ products/views.py
@@ -8,6 +8,7 @@ from .forms import ProductForm
 
 
 def all_products(request):
+    """ Searching or sorting products """
 
     products = Product.objects.all()
     query = None
@@ -78,13 +79,37 @@ def add_product(request):
             messages.success(request, 'The product was added successfully')
             return redirect(reverse('add_product'))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product is updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
