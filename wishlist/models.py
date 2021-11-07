dc86573722d9d81f4c4d9625132aec2ce87b38be
kristinabog <kristinabog7@hotmail.com>
2021-11-07 19:54:40 +0000

    Added wishlist app

---
+++ wishlist/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+
+class WishList(models.Model):
+    """
+    Model with the list of wishlist items of the user
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    wishlist_items = models.ManyToManyField(Product,
+                                            related_name='wishlist_products')
+
+    def __str__(self):
+        return f'WishList - {self.user}'
