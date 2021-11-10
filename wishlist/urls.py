395cee20de1e69646f020e3d9f8fd56bc9a6c1c4
kristinabog <kristinabog7@hotmail.com>
2021-11-10 00:24:49 +0000

    Add add to bag url

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.get_wishlist, name='wishlist'),
+    path(
+        'add_to_wishlist/<product_id>',
+        views.add_to_wishlist,
+        name='add_to_wishlist'),
 ]
