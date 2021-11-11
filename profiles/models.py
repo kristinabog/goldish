7743f3e610de050b8e20dc5a9b1047e485f7b9f1
kristinabog <kristinabog7@hotmail.com>
2021-11-11 20:06:28 +0000

    Added Delivery address model and migrations

--- profiles/models.py
+++ profiles/models.py
@@ -38,3 +38,22 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     # Existing users: just saving the profile
     instance.userprofile.save()
+
+
+class DeliveryAddress(models.Model):
+    """
+    Model to store multiple delivery addresses
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(
+        max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Country *', null=True, blank=True)
