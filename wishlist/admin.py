97db1aa07d830c97676eed2d683e1b42af525e01
kristinabog <kristinabog7@hotmail.com>
2021-11-11 20:05:00 +0000

    Added docstring

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -3,6 +3,10 @@ from .models import Wishlist
 
 
 class WishlistAdmin(admin.ModelAdmin):
+    """
+    View the wishlist model in admin database
+    """
     list_display = ('__str__'),
+
 
 admin.site.register(Wishlist, WishlistAdmin)
