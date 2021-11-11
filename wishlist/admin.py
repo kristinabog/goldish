24cb59f7d79374dc7b9e10e8ce35d2fb94b2653b
kristinabog <kristinabog7@hotmail.com>
2021-11-11 19:55:58 +0000

    Added wishlist admin class

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Wishlist
 
-# Register your models here.
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ('__str__'),
+
+admin.site.register(Wishlist, WishlistAdmin)
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
+from django.contrib import messages
+
+from products.models import Product
 from .models import Wishlist
 
 
@@ -11,8 +14,36 @@ def get_wishlist(request):
     """
     wishlist = Wishlist.objects.filter(user=request.user)
 
+    context = {
+        'wishlist': wishlist,
+    }
+
+    return render(request, 'wishlist/wishlist.html', context)
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """
+    Add a product to the user's wishlist
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    wishlist = Wishlist.objects.get(user=request.user)
+
+    if wishlist:
+        if product in wishlist:
+        messages.error(request, "Item is already in your wishlist")
+        else:
+        wishlist.save(product)
+        messages.success(
+            request, f'Added {product.name} to your wishlist')
+    else:
+
+
+
     template = 'wishlist/wishlist.html'
     context = {
+        'product': product,
         'wishlist': wishlist,
     }
 
