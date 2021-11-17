c6e120dc6a2bec03285f68128f539561a472d471
kristinabog <kristinabog7@hotmail.com>
2021-11-17 19:28:35 +0000

    Remove anything related to DeliveryAddress model part 2

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile, DeliveryAddress
+from .models import UserProfile
 
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('__str__'),
 
 
-class DeliveryAddressAdmin(admin.ModelAdmin):
-    list_display = ('__str__'),
-
-
 admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(DeliveryAddress, DeliveryAddressAdmin)
