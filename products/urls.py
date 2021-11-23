5495bf1bbae0aae92a1c5f2daf395fce4ac0549f
kristinabog <kristinabog7@hotmail.com>
2021-11-23 00:36:52 +0000

    Resolve line too long errors

--- products/urls.py
+++ products/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('<int:product_id>/add_review/', views.add_review, name='add_review'),
     path('edit_review/<int:review_id>/',
          views.edit_review, name='edit_review'),
-    path('delete_review/<int:review_id>/', views.delete_review, name='delete_review'),
+    path('delete_review/<int:review_id>/',
+         views.delete_review, name='delete_review'),
 ]
