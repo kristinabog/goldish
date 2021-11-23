98954700aac5e706cea25eaefd0d6362f190b571
kristinabog <kristinabog7@hotmail.com>
2021-11-23 16:48:04 +0000

    add error pages

---
+++ goldish/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def error_404(request, exception):
+    return render(request, 'errors/404.html')
+
+
+def error_500(request):
+    return render(request, 'errors/500.html')
