a606bf75f1c9b15d3c91207fdbef4ec4570c40bb
kristinabog <kristinabog7@hotmail.com>
2022-11-21 23:31:58 +0000

    fix indentation bug

--- goldish/settings.py
+++ goldish/settings.py
@@ -34,10 +34,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['kb-goldish.herokuapp.com', 'localhost']
 
- # Add Render.com URL to allowed hosts
- RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
- if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
